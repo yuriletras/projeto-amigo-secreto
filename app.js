794116d16369346220a53476dff586c05add5df4
yuri <yuri_bs@hotmail.com>
2025-01-20 09:29:02 -0300

    envio de dados commit 1

---
+++ app.js
@@ -0,0 +1,47 @@
+let nomes = [];
+
+const botaoAdicionar = document.querySelector('.button-add');
+botaoAdicionar.addEventListener('click', adicionarAmigo);
+
+function adicionarAmigo() {
+    const nome = document.getElementById("amigo").value;
+    if (nome === "") {
+        alert("Digite um nome válido!");
+        return;
+    }
+    nomes.push(nome);
+    atualizarLista();
+    document.getElementById("amigo").value = "";
+}
+
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    nomes.forEach(nome => {
+        const li = document.createElement("li");
+        li.textContent = nome;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (nomes.length === 0) {
+        alert("Adicione pelo menos um nome");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * nomes.length);
+    const amigoSecreto = nomes[indiceAleatorio];
+    nomes.splice(indiceAleatorio, 1);
+
+    document.getElementById("resultado").textContent = `O amigo secreto é ${amigoSecreto}`;
+    atualizarLista();
+}
+
+function reiniciarJogo() {
+    if (confirm("Tem certeza que deseja reiniciar o jogo?")) {
+        nomes = [];
+        atualizarLista();
+        document.getElementById("resultado").textContent = "";
+    }
+}
